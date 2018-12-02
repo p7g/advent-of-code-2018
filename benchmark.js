f04ce60866e43005fb464320fe47ffa1d129c9ce
Patrick Gingras <775.pg.12@gmail.com>
2018-12-02 18:05:03 -0500

    streamlined benchmark-adding process

--- benchmark.js
+++ benchmark.js
@@ -1,5 +1,4 @@
 const Benchmark = require('benchmark');
-const newLine = () => process.stdout.write('\n');
 
 const totalFrequency = require('./day1/totalFrequency');
 const repeatedFrequency = require('./day1/repeatedFrequency');
@@ -7,22 +6,15 @@ const repeatedFrequency = require('./day1/repeatedFrequency');
 const checksum = require('./day2/checksum');
 const commonLetters = require('./day2/commonLetters');
 
-process.stdout.write('day 1\n');
-new Benchmark.Suite('day 1')
-  .add('total frequency', totalFrequency)
-  .add('repeated frequency', repeatedFrequency)
-  .on('cycle', e => process.stdout.write(
+function doSuite(name, functions) {
+  process.stdout.write(`${name}\n`);
+  const s = new Benchmark.Suite(name);
+  functions.forEach(fn => s.add(fn.name, fn));
+  s.on('cycle', e => process.stdout.write(
     `${e.target} (${(e.target.times.period * 1000).toFixed(2)}ms)\n`,
-  ))
-  .run({ async: false });
-newLine();
+  )).run({ async: false });
+  process.stdout.write('\n');
+}
 
-process.stdout.write('day 2\n');
-new Benchmark.Suite('day 2')
-  .add('checksum', checksum)
-  .add('common letters', commonLetters)
-  .on('cycle', e => process.stdout.write(
-    `${e.target} (${(e.target.times.period * 1000).toFixed(2)}ms)\n`,
-  ))
-  .run({ async: false });
-newLine();
+doSuite('Day 1', [totalFrequency, repeatedFrequency]);
+doSuite('Day 2', [checksum, commonLetters]);
