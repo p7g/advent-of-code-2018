ed9eecb838da6425600264d3eca74b03f5d87015
Patrick Gingras <775.pg.12@gmail.com>
2018-12-02 16:30:53 -0500

    made checksum faster

--- day2/checksum.js
+++ day2/checksum.js
@@ -1,19 +1,32 @@
 const { readFileSync } = require('fs');
 
+const start = Date.now();
 const ids = readFileSync('./input.txt')
   .toString()
   .trim()
   .split('\n');
 
 function countLetters(str) {
-  const letters = {};
+  /* eslint-disable */
+  const letters = {
+    a: 0, b: 0, c: 0, d: 0, e: 0, f: 0,
+    g: 0, h: 0, i: 0, j: 0, k: 0, l: 0,
+    m: 0, n: 0, o: 0, p: 0, q: 0, r: 0,
+    s: 0, t: 0, u: 0, v: 0, w: 0, x: 0,
+    y: 0, z: 0,
+  };
+  /* eslint-enable */
   for (let i = 0; i < str.length; i += 1) {
-    letters[str[i]] = letters[str[i]] + 1 || 1;
+    const letter = str[i];
+    if (letters[letter]) {
+      letters[letter] += 1;
+    } else {
+      letters[letter] = 1;
+    }
   }
   return letters;
 }
 
-const start = Date.now();
 const [double, triple] = ids.reduce(([twice, thrice], id) => {
   const letters = Object.values(countLetters(id));
   let two = twice;
