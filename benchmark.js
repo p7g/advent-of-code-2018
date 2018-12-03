83b2a468aae92218cf978950ebe3c7dd22dd83f8
Patrick Gingras <775.pg.12@gmail.com>
2018-12-02 19:22:01 -0500

    automated benchmark thing

--- benchmark.js
+++ benchmark.js
@@ -1,3 +1,5 @@
+const { readdirSync } = require('fs');
+const { join } = require('path');
 const Benchmark = require('benchmark');
 
 function doSuite(name, functions) {
@@ -10,7 +12,16 @@ function doSuite(name, functions) {
   process.stdout.write('\n');
 }
 
-/* eslint-disable */
+const days = readdirSync(join(__dirname, 'days'));
+days.forEach((day) => {
+  const name = `day ${day}`;
+  const dir = join(__dirname, 'days', day);
+  const parts = readdirSync(dir).filter(file => file.endsWith('.js'));
+  // eslint-disable-next-line
+  doSuite(name, parts.map((file) => require(join(dir, file))));
+});
+
+/*
 doSuite('Day 1', [
   require('./days/1/totalFrequency'),
   require('./days/1/repeatedFrequency'),
@@ -19,4 +30,4 @@ doSuite('Day 2', [
   require('./days/2/checksum'),
   require('./days/2/commonLetters'),
 ]);
-/* eslint-enable */
+*/
