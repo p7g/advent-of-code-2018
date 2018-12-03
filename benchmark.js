343964254385e7d376f3139d33b04828e25ab150
Patrick Gingras <775.pg.12@gmail.com>
2018-12-03 17:25:52 -0500

    added command-line interface

--- benchmark.js
+++ benchmark.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
 const { readdirSync } = require('fs');
 const { join } = require('path');
 const Benchmark = require('benchmark');
@@ -7,16 +10,40 @@ function doSuite(name, functions) {
   const s = new Benchmark.Suite(name);
   functions.forEach(fn => s.add(fn.name, fn));
   s.on('cycle', e => process.stdout.write(
-    `${e.target} (${(e.target.times.period * 1000).toFixed(2)}ms)\n`,
+    `${e.target} = ${JSON.stringify(e.target.fn())} (${
+      (e.target.times.period * 1000).toFixed(2)
+    }ms)\n`,
   )).run({ async: false });
   process.stdout.write('\n');
 }
 
 const days = readdirSync(join(__dirname, 'days'));
-days.forEach((day) => {
-  const name = `day ${day}`;
-  const dir = join(__dirname, 'days', day);
-  const parts = readdirSync(dir).filter(file => file.endsWith('.js'));
-  // eslint-disable-next-line
-  doSuite(name, parts.map((file) => require(join(dir, file))));
-});
+
+const args = yargs
+  .usage('$0 <cmd> [args]')
+  .command('run [day]', 'Benchmark the files for the given day', (yrgs) => {
+    yrgs.positional('day', {
+      type: 'number',
+      choices: [null, ...days.map(d => Number.parseInt(d, 10))],
+      default: null,
+      describe: 'the day to benchmark, or all if none provided',
+    });
+  }, ({ day: d }) => {
+    ([d.toString()] || days).forEach((day) => {
+      const name = `day ${day}`;
+      const dir = join(__dirname, 'days', day);
+      const parts = readdirSync(dir).filter(file => file.endsWith('.js'));
+      // eslint-disable-next-line
+      doSuite(name, parts.map(file => require(join(dir, file))));
+    });
+  })
+  .demandCommand()
+  .recommendCommands()
+  .showHelpOnFail(true)
+  .strict()
+  .help()
+  .argv;
+
+if (!args._[0]) {
+  yargs.showHelp();
+}
